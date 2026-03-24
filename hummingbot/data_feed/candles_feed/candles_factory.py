d435a23fb6958bc775a12c4f25d3ae04e24d2c81
Laban <oktosilaban@gmail.com>
2026-03-24 21:02:51 +0700

    Add Lighter Perpetual to Candles Factory

--- hummingbot/data_feed/candles_feed/candles_factory.py
+++ hummingbot/data_feed/candles_feed/candles_factory.py
@@ -23,6 +23,9 @@ from hummingbot.data_feed.candles_feed.hyperliquid_perpetual_candles.hyperliquid
 from hummingbot.data_feed.candles_feed.hyperliquid_spot_candles.hyperliquid_spot_candles import HyperliquidSpotCandles
 from hummingbot.data_feed.candles_feed.kraken_spot_candles.kraken_spot_candles import KrakenSpotCandles
 from hummingbot.data_feed.candles_feed.kucoin_perpetual_candles.kucoin_perpetual_candles import KucoinPerpetualCandles
+from hummingbot.data_feed.candles_feed.lighter_perpetual_candles.lighter_perpetual_candles import (
+    LighterPerpetualCandles,
+)
 from hummingbot.data_feed.candles_feed.kucoin_spot_candles.kucoin_spot_candles import KucoinSpotCandles
 from hummingbot.data_feed.candles_feed.mexc_perpetual_candles.mexc_perpetual_candles import MexcPerpetualCandles
 from hummingbot.data_feed.candles_feed.mexc_spot_candles.mexc_spot_candles import MexcSpotCandles
@@ -57,6 +60,7 @@ class CandlesFactory:
         "gate_io_perpetual": GateioPerpetualCandles,
         "kucoin": KucoinSpotCandles,
         "kucoin_perpetual": KucoinPerpetualCandles,
+        "lighter_perpetual": LighterPerpetualCandles,
         "ascend_ex": AscendExSpotCandles,
         "okx_perpetual": OKXPerpetualCandles,
         "okx": OKXSpotCandles,
